7e39597577f51ff742fa31b5bf38739ce435ccc0
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-11 14:20:58 +0300

    added all methods to CategoryController and ProductController

---
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package mate.academy.springboot.datajpa.controller;
+
+import javax.validation.Valid;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.dto.request.CategoryRequestDto;
+import mate.academy.springboot.datajpa.model.dto.response.CategoryResponseDto;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import mate.academy.springboot.datajpa.service.mapper.CategoryMapper;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @PostMapping
+    public CategoryResponseDto add(@RequestBody @Valid CategoryRequestDto requestDto) {
+        Category category = categoryMapper.mapToModel(requestDto);
+        categoryService.save(category);
+        return categoryMapper.mapToDto(category);
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponseDto getCategoryById(@PathVariable Long id) {
+        return categoryMapper.mapToDto(categoryService.getCategoryById(id));
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponseDto update(@PathVariable Long id,
+                                      @RequestBody @Valid CategoryRequestDto categoryRequestDto) {
+        Category category = categoryMapper.mapToModel(categoryRequestDto);
+        category.setId(id);
+        return categoryMapper.mapToDto(categoryService.save(category));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/mapper/ProductMapper.java
@@ -0,0 +1,36 @@
+package mate.academy.springboot.datajpa.service.mapper;
+
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.model.dto.request.ProductRequestDto;
+import mate.academy.springboot.datajpa.model.dto.response.ProductResponseDto;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper implements RequestDtoMapper<ProductRequestDto, Product>,
+                                        ResponseDtoMapper<ProductResponseDto, Product> {
+    private final CategoryService categoryService;
+
+    public ProductMapper(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public Product mapToModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        product.setCategory(categoryService.getCategoryById(dto.getCategoryId()));
+        return product;
+    }
+
+    @Override
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto responseDto = new ProductResponseDto();
+        responseDto.setId(product.getId());
+        responseDto.setTitle(product.getTitle());
+        responseDto.setPrice(product.getPrice());
+        responseDto.setCategoryId(product.getCategory().getId());
+        return responseDto;
+    }
+}
