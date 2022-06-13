53059003624e05ccac4b335c9bb1a18674c4b2f5
djmolot <djmolot@gmail.com>
2022-06-13 22:24:49 +0200

    removed unnecessary comment

--- src/main/java/mate/academy/springboot/datajpa/repository/ProductRepository.java
+++ src/main/java/mate/academy/springboot/datajpa/repository/ProductRepository.java
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    //@Query(value = "from Product p where p.price between ?1 and ?2")
     List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to);
 
     List<Product> findAllByCategoryIdIn(Set<Long> categoryIds);
-
 }
