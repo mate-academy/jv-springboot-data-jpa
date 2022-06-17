ad79f87b79ca37692a73c8f93f32dfbeeeb71f98
Iryna Hrynda <hryndairyna1@gmail.com>
2022-06-17 22:31:28 +0300

    - implemented spring data jpa

---
+++ src/main/java/mate/academy/springboot/datajpa/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.springboot.datajpa.mapper;
+
+import mate.academy.springboot.datajpa.dto.request.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.response.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public CategoryResponseDto mapToDto(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        return categoryResponseDto;
+    }
+
+    public Category mapToModel(CategoryRequestDto categoryRequestDto) {
+        Category category = new Category();
+        category.setName(categoryRequestDto.getName());
+        return category;
+    }
+}
