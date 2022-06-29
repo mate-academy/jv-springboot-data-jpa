e26a462b2aae1a6083688fff7176817cce958ade
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-06-30 01:03:00 +0300

    Finally works

--- src/main/java/mate/academy/springboot/datajpa/repository/ProductRepository.java
+++ src/main/java/mate/academy/springboot/datajpa/repository/ProductRepository.java
@@ -4,11 +4,13 @@ import java.math.BigDecimal;
 import java.util.List;
 import mate.academy.springboot.datajpa.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> getProductsByPriceBetween(BigDecimal from, BigDecimal to);
 
+    @Query("SELECT p FROM Product p WHERE p.category IN p.category")
     List<Product> getProductsByCategoryIn(List<Long> categoryIds);
 }
