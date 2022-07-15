413c6b6dd663af7249fd6d1fe1bc16577722bd44
Maksym Pokras <mxpokras@gmail.com>
2022-07-15 17:59:44 +0200

    Product model added

---
+++ src/main/java/mate/academy/springboot/datajpa/model/Product.java
@@ -0,0 +1,24 @@
+package mate.academy.springboot.datajpa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="categories")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    @ManyToOne
+    private Category category;
+}
