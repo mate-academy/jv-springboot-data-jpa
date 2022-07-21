1dd7f6eb4ad8c814976eaed961cbf1a780ae5a4b
Viktoriia Haraniuk <87524717+viktoriia-haraniuk@users.noreply.github.com>
2022-07-21 13:13:29 +0300

    Update src/main/java/mate/academy/springboot/datajpa/controllers/ProductController.java
    Co-authored-by: Liliia Ponomarenko <96740815+liliia-ponomarenko@users.noreply.github.com>

--- src/main/java/mate/academy/springboot/datajpa/controllers/ProductController.java
+++ src/main/java/mate/academy/springboot/datajpa/controllers/ProductController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Data
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/products")
 public class ProductController {
