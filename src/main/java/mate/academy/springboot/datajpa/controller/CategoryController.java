d7742c65d514b91b177e08eca59a6af8d4f8888b
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-08-16 23:24:57 +0300

    first commit

--- src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,7 @@ public class CategoryController {
         return categoryMapper.mapToDto(categoryService.getById(id));
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public CategoryResponseDto update(@PathVariable Long id, @RequestBody CategoryRequestDto dto) {
         Category category = categoryMapper.mapToModel(dto);
         category.setId(id);
