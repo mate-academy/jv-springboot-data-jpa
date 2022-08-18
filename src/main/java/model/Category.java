6bf4a79a43a29c25b95747985d1c4819794046d3
Andrey Chumak <chumakandrey7@gmail.com>
2022-08-18 19:33:14 +0300

    Replaced @Data in entity classes with other annotation Renamed dtoMappers methods Added @RequiredArgsConstructor to controllers classes

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -1,17 +1,14 @@
 package model;
 
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
 @Getter
 @Setter
--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,12 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
 @Getter
 @Setter
