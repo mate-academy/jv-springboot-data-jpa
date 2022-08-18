a2a794d15312298cc55939f45a4dca8765cf7b30
Vasylyi Bevz <bevzvasya@gmail.com>
2022-08-18 12:11:22 +0300

    Spring boot data jpa was done іеstep 1

--- src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
-
     private final CategoryService categoryService;
     private final CategoryMapper categoryMapper;
 
