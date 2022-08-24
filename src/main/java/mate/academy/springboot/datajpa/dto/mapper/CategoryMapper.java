efe45b6bd1ce75804ab838f65a3764e031a89cf9
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-08-24 23:32:06 +0300

    implemented tasks

---
+++ src/main/java/mate/academy/springboot/datajpa/dto/mapper/CategoryMapper.java
@@ -0,0 +1,29 @@
+package mate.academy.springboot.datajpa.dto.mapper;
+
+import mate.academy.springboot.datajpa.dto.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.repository.CategoryRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryMapper(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public CategoryResponseDto toDto(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        return categoryResponseDto;
+    }
+
+    public Category toModel(CategoryRequestDto categoryRequestDto) {
+        Category category = new Category();
+        category.setName(categoryRequestDto.getName());
+        return category;
+    }
+}
