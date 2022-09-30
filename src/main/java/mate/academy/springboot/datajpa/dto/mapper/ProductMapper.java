82057eeee38c22054f907e64dd54ffc880994688
Oleksandr Domnich <dmnichz@gmail.com>
2022-09-30 16:21:44 +0300

    added solution for jv-springboot-data-jpa task

---
+++ src/main/java/mate/academy/springboot/datajpa/dto/mapper/ProductMapper.java
@@ -0,0 +1,26 @@
+package mate.academy.springboot.datajpa.dto.mapper;
+
+import mate.academy.springboot.datajpa.dto.request.ProductRequestDto;
+import mate.academy.springboot.datajpa.dto.response.ProductResponseDto;
+import mate.academy.springboot.datajpa.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public Product mapToModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        product.setCategory(dto.getCategory());
+        return product;
+    }
+
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto responseDto = new ProductResponseDto();
+        responseDto.setId(product.getId());
+        responseDto.setTitle(product.getTitle());
+        responseDto.setPrice(product.getPrice());
+        responseDto.setCategory(product.getCategory());
+        return responseDto;
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/ProductServiceImpl.java
@@ -0,0 +1,60 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.repository.ProductRepository;
+import mate.academy.springboot.datajpa.repository.specification.ProductSpecificationManager;
+import mate.academy.springboot.datajpa.service.ProductService;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ProductSpecificationManager productSpecificationManager;
+
+    public ProductServiceImpl(ProductRepository productRepository,
+                              ProductSpecificationManager productSpecificationManager) {
+        this.productRepository = productRepository;
+        this.productSpecificationManager = productSpecificationManager;
+    }
+
+    @Override
+    public Product add(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product get(Long id) {
+        return productRepository.getById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllByPriceBetween(BigDecimal from, BigDecimal to) {
+        return productRepository.getAllByPriceBetween(from, to);
+    }
+
+    @Override
+    public List<Product> getAllByCategories(Map<String, String> params) {
+        Specification<Product> specification = null;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            Specification<Product> sp = productSpecificationManager.get(entry.getKey(),
+                    entry.getValue().split(","));
+            specification = specification == null
+                    ? Specification.where(sp) : specification.and(sp);
+        }
+        return productRepository.findAll(specification);
+    }
+}
