0105f4b43a685e0fb904f1fa93d515676edf6db2
Nazar <litvik.12@gmail.com>
2022-10-22 02:40:12 +0300

    used Spring Boot for working with web also used analogue of CriteriaQuery in SpringBoot

---
+++ src/main/java/mate/academy/springboot/datajpa/repository/specification/product/ProductCategoryInSpecification.java
@@ -0,0 +1,33 @@
+package mate.academy.springboot.datajpa.repository.specification.product;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.repository.specification.SpecificationProvider;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductCategoryInSpecification implements SpecificationProvider<Product> {
+    private static final String FILTER_KEY = "categoryIn";
+    private static final String FIELD_NAME = "category";
+
+    @Override
+    public Specification<Product> getSpecification(String[] categories) {
+        return ((root, query, cb) -> {
+            Join<Product, Category> join = root.join(FIELD_NAME, JoinType.INNER);
+            CriteriaBuilder.In<String> predicate = cb.in(join.get(FIELD_NAME));
+            for (String value : categories) {
+                predicate.value(value);
+            }
+            return cb.and(predicate, predicate);
+        });
+    }
+
+    @Override
+    public String getFilterKey() {
+        return FILTER_KEY;
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.springboot.datajpa.service.mapper;
+
+import mate.academy.springboot.datajpa.dto.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public CategoryResponseDto toDto(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        return categoryResponseDto;
+    }
+
+    public Category fromDto(CategoryRequestDto categoryRequest) {
+        Category category = new Category();
+        category.setName(category.getName());
+        return category;
+    }
+}
