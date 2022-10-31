66afee4100b0a480eb9b6fa0fc559d4cf079750d
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-10-31 10:03:23 +0200

    method get was implemented for category and product controllers

--- src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -3,8 +3,10 @@ package mate.academy.springboot.datajpa.controller;
 import mate.academy.springboot.datajpa.dto.request.CategoryRequestDto;
 import mate.academy.springboot.datajpa.dto.response.CategoryResponseDto;
 import mate.academy.springboot.datajpa.model.Category;
-import mate.academy.springboot.datajpa.repository.CategoryRepository;
+import mate.academy.springboot.datajpa.service.CategoryService;
 import mate.academy.springboot.datajpa.service.mapper.CategoryMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,19 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
     private final CategoryMapper categoryMapper;
 
-    public CategoryController(CategoryRepository categoryRepository,
-                              CategoryMapper categoryMapper) {
-        this.categoryRepository = categoryRepository;
+    public CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
         this.categoryMapper = categoryMapper;
     }
 
-
     @PostMapping
     public CategoryResponseDto add(@RequestBody CategoryRequestDto requestDto) {
-        Category category = categoryRepository.save(categoryMapper.mapToModel(requestDto));
+        Category category = categoryService.save(categoryMapper.mapToModel(requestDto));
+        return categoryMapper.mapToDto(category);
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponseDto get(@PathVariable Long id) {
+        Category category = categoryService.get(id);
         return categoryMapper.mapToDto(category);
     }
 }
--- src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
@@ -3,8 +3,10 @@ package mate.academy.springboot.datajpa.controller;
 import mate.academy.springboot.datajpa.dto.request.ProductRequestDto;
 import mate.academy.springboot.datajpa.dto.response.ProductResponseDto;
 import mate.academy.springboot.datajpa.model.Product;
-import mate.academy.springboot.datajpa.repository.ProductRepository;
+import mate.academy.springboot.datajpa.service.ProductService;
 import mate.academy.springboot.datajpa.service.mapper.ProductMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    private final ProductRepository productRepository;
+    private final ProductService productService;
     private final ProductMapper productMapper;
 
-    public ProductController(ProductRepository productRepository,
+    public ProductController(ProductService productService,
                              ProductMapper productMapper) {
-        this.productRepository = productRepository;
+        this.productService = productService;
         this.productMapper = productMapper;
     }
 
     @PostMapping
     public ProductResponseDto add(@RequestBody ProductRequestDto requestDto) {
-        Product product = productRepository.save(productMapper.mapToModel(requestDto));
+        Product product = productService.save(productMapper.mapToModel(requestDto));
+        return productMapper.mapToDto(product);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponseDto get(@PathVariable Long id) {
+        Product product = productService.get(id);
         return productMapper.mapToDto(product);
     }
 
