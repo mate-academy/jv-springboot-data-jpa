f2b40162f93cca1617ffb59e567ff2e9849285fa
Anna Popova <anitapopova1996@gmail.com>
2022-11-02 19:02:03 +0200

    Created dto

---
+++ src/main/java/mate/academy/springboot/datajpa/dto/request/RequestCategoryDto.java
@@ -0,0 +1,13 @@
+package mate.academy.springboot.datajpa.dto.request;
+
+public class RequestCategoryDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/dto/request/RequestProductDto.java
@@ -0,0 +1,33 @@
+package mate.academy.springboot.datajpa.dto.request;
+
+import java.math.BigDecimal;
+
+public class RequestProductDto {
+    private String title;
+    private BigDecimal price;
+    private Long categoryId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
