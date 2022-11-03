29f1493efda703b285167aaca96677a55ae480c0
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-11-03 21:12:50 +0100

    Add models Product and Categories

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,12 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/model/Product.java
@@ -0,0 +1,21 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    @OneToOne
+    private Category category;
+}
