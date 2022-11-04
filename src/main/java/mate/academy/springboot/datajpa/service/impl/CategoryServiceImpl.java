3368d14b5b7a3ec336e449ecd4f97c787ce37cec
Misha Brovkin <haasiakira@gmail.com>
2022-11-04 10:14:59 +0300

    improve code style

--- src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-
     private final CategoryRepository repository;
 
     public CategoryServiceImpl(CategoryRepository repository) {
