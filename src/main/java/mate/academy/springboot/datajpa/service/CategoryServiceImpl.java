a78207c7270fab4cca80b5d63ae319abcd96c321
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-11-04 08:41:33 +0100

    Add CategoryService

---
+++ src/main/java/mate/academy/springboot/datajpa/service/CategoryServiceImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.springboot.datajpa.service;
+
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CategoryServiceImpl implements CategoryService {
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category create(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category getById(Long id) {
+        return categoryRepository.getById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Category update(Category category) {
+        return categoryRepository.save(category);
+    }
+}
