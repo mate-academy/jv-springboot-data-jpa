63b7d94369e06af0b31d9ce09a01e5b050c5c69c
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-11-05 11:14:37 +0200

    Added services

---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,6 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import mate.academy.springboot.datajpa.service.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/ProductServiceImpl.java
@@ -0,0 +1,6 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import mate.academy.springboot.datajpa.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+}
