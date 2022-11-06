f6e464538975676ae3619e199f5fb59d0b7810ff
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-11-06 15:22:11 +0200

    Refactored REST ProductController

--- src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
@@ -15,7 +15,6 @@ import mate.academy.springboot.datajpa.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -52,8 +51,8 @@ public class ProductController {
         return productResponseDtoMapper.toDto(productService.save(product));
     }
 
-    @GetMapping("/{product_id}")
-    public ProductResponseDto getById(@PathVariable(name = "product_id") Long productId) {
+    @GetMapping
+    public ProductResponseDto getById(@RequestParam(name = "product_id") Long productId) {
         return productResponseDtoMapper.toDto(productService.getById(productId));
     }
 
