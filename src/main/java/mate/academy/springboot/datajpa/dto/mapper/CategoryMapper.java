519682ca0b09dfbdd8b767ddc915ce4a2131521e
Ilya <zimmboco@gmail.com>
2022-11-08 10:41:37 +0200

    add product and category

---
+++ src/main/java/mate/academy/springboot/datajpa/dto/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.springboot.datajpa.dto.mapper;
+
+import mate.academy.springboot.datajpa.dto.category.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.category.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public CategoryResponseDto toResponseDto(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        return categoryResponseDto;
+    }
+
+    public Category toModel(CategoryRequestDto requestDto) {
+        Category category = new Category();
+        category.setName(requestDto.getName());
+        return category;
+    }
+}
