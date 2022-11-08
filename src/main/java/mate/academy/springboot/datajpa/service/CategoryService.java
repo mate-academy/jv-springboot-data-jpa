4ae008c36e30a5eb4b408a88d905cfd1090e1cfc
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-11-08 21:16:53 +0200

    remove reduntant annotation

--- src/main/java/mate/academy/springboot/datajpa/service/CategoryService.java
+++ src/main/java/mate/academy/springboot/datajpa/service/CategoryService.java
@@ -1,9 +1,7 @@
 package mate.academy.springboot.datajpa.service;
 
 import mate.academy.springboot.datajpa.model.Category;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CategoryService {
     Category create(Category category);
 
