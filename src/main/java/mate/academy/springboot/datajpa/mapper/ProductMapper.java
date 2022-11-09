52a25f4dc5ef9c2e6923eb4989c43daa7cc64376
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-11-09 14:13:35 +0200

    hw done

---
+++ src/main/java/mate/academy/springboot/datajpa/mapper/ProductMapper.java
@@ -0,0 +1,33 @@
+package mate.academy.springboot.datajpa.mapper;
+
+import mate.academy.springboot.datajpa.dto.product.ProductRequestDto;
+import mate.academy.springboot.datajpa.dto.product.ProductResponseDto;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.repository.CategoryRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    private final CategoryRepository categoryRepository;
+
+    public ProductMapper(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public ProductResponseDto toDto(Product product) {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setTitle(product.getTitle());
+        productResponseDto.setPrice(product.getPrice());
+        productResponseDto.setCategoryId(product.getCategory().getId());
+        return productResponseDto;
+    }
+
+    public Product toModel(ProductRequestDto productRequestDto) {
+        Product product = new Product();
+        product.setTitle(productRequestDto.getTitle());
+        product.setPrice(productRequestDto.getPrice());
+        product.setCategory(categoryRepository.getById(productRequestDto.getCategoryId()));
+        return product;
+    }
+}
