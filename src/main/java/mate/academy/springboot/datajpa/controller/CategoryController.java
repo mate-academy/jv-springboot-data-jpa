1dfe4fdccf51131f301060b5d6e115e68da24446
Maksym Korkosh <makskorkosh@gmail.com>
2022-11-09 09:26:43 +0200

    added solution

---
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -0,0 +1,56 @@
+package mate.academy.springboot.datajpa.controller;
+
+import mate.academy.springboot.datajpa.dto.request.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.response.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import mate.academy.springboot.datajpa.service.mapper.CategoryMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @PostMapping
+    public CategoryResponseDto create(@RequestBody CategoryRequestDto requestDto) {
+        Category category = categoryMapper.toModel(requestDto);
+        category = categoryService.save(category);
+        return categoryMapper.toDto(category);
+    }
+
+    @GetMapping("/{categoryId}")
+    public CategoryResponseDto getById(@PathVariable Long categoryId) {
+        Category category = categoryService.getById(categoryId);
+        return categoryMapper.toDto(category);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void delete(@PathVariable Long categoryId) {
+        categoryService.delete(categoryId);
+    }
+
+    @PutMapping("/{categoryId}")
+    public CategoryResponseDto update(@PathVariable Long categoryId,
+                                      @RequestBody CategoryRequestDto requestDto) {
+        Category category = categoryMapper.toModel(requestDto);
+        category.setId(categoryId);
+        return categoryMapper.toDto(categoryService.update(category));
+    }
+}
