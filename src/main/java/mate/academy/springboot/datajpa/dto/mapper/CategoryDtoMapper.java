3f73917ab89fae495d31fbc58d601d24bfeeec65
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-12-09 22:55:05 +0100

    Hw-done

---
+++ src/main/java/mate/academy/springboot/datajpa/dto/mapper/CategoryDtoMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.springboot.datajpa.dto.mapper;
+
+import mate.academy.springboot.datajpa.dto.request.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.response.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryDtoMapper {
+    public CategoryResponseDto toDto(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        return categoryResponseDto;
+    }
+
+    public Category fromDto(CategoryRequestDto categoryRequestDto) {
+        Category category = new Category();
+        category.setName(categoryRequestDto.getName());
+        return category;
+    }
+}
