d67c9a8458dcdba7579929040795888881a83c49
Alina Nikiforova <0500302717a@gmail.com>
2022-12-10 16:28:46 +0100

    added

--- src/main/java/mate/academy/springboot/datajpa/model/Product.java
+++ src/main/java/mate/academy/springboot/datajpa/model/Product.java
@@ -1,8 +1,12 @@
 package mate.academy.springboot.datajpa.model;
 
 import java.math.BigDecimal;
-import javax.persistence.*;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Data
