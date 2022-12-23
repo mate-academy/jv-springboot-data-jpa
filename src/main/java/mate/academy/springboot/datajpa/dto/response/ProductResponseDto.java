915c97c27f83fa3a501e5208281091b596adf953
EvgenyTyshchenko <evgeny.tyshchenko@gmail.com>
2022-12-23 18:01:28 +0200

    Springboot data jpa solution. v2

--- src/main/java/mate/academy/springboot/datajpa/dto/response/ProductResponseDto.java
+++ src/main/java/mate/academy/springboot/datajpa/dto/response/ProductResponseDto.java
@@ -2,12 +2,11 @@ package mate.academy.springboot.datajpa.dto.response;
 
 import java.math.BigDecimal;
 import lombok.Data;
-import mate.academy.springboot.datajpa.model.Category;
 
 @Data
 public class ProductResponseDto {
     private Long id;
     private String title;
     private BigDecimal price;
-    private Category category;
+    private Long categoryId;
 }
--- src/main/java/mate/academy/springboot/datajpa/service/mapper/impl/ProductMapper.java
+++ src/main/java/mate/academy/springboot/datajpa/service/mapper/impl/ProductMapper.java
@@ -3,6 +3,7 @@ package mate.academy.springboot.datajpa.service.mapper.impl;
 import mate.academy.springboot.datajpa.dto.request.ProductRequestDto;
 import mate.academy.springboot.datajpa.dto.response.ProductResponseDto;
 import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.service.CategoryService;
 import mate.academy.springboot.datajpa.service.mapper.RequestDtoMapper;
 import mate.academy.springboot.datajpa.service.mapper.ResponseDtoMapper;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductMapper implements ResponseDtoMapper<ProductResponseDto, Product>,
         RequestDtoMapper<ProductRequestDto, Product> {
+    private final CategoryService categoryService;
+
+    public ProductMapper(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @Override
     public Product mapToModel(ProductRequestDto productRequestDto) {
         Product product = new Product();
         product.setTitle(productRequestDto.getTitle());
         product.setPrice(productRequestDto.getPrice());
-        product.setCategory(productRequestDto.getCategory());
+        product.setCategory(categoryService.get(productRequestDto.getCategoryId()));
         return product;
     }
 
@@ -25,7 +32,7 @@ public class ProductMapper implements ResponseDtoMapper<ProductResponseDto, Prod
         productResponseDto.setId(product.getId());
         productResponseDto.setTitle(productResponseDto.getTitle());
         productResponseDto.setPrice(product.getPrice());
-        productResponseDto.setCategory(product.getCategory());
+        productResponseDto.setCategoryId(product.getCategory().getId());
         return productResponseDto;
     }
 }
