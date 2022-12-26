c66acabad32dc277153eedbc33f0ce14dc493a00
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-12-26 16:58:22 +0100

    initial3

--- src/main/java/mate/academy/springboot/datajpa/repository/specification/product/ProductCategoryIdInSpecification.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.springboot.datajpa.repository.specification.product;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ProductCategoryIdInSpecification {
-}
