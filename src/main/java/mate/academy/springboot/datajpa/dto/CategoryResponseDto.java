31491784f2369d1a458532e5a258fac727ca45ce
Nazarii Pertsak <pertsak98@gmail.com>
2022-12-26 17:48:42 +0200

    deleted some imports

--- src/main/java/mate/academy/springboot/datajpa/dto/CategoryResponseDto.java
+++ src/main/java/mate/academy/springboot/datajpa/dto/CategoryResponseDto.java
@@ -2,7 +2,6 @@ package mate.academy.springboot.datajpa.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
--- src/main/java/mate/academy/springboot/datajpa/dto/ProductResponseDto.java
+++ src/main/java/mate/academy/springboot/datajpa/dto/ProductResponseDto.java
@@ -3,7 +3,6 @@ package mate.academy.springboot.datajpa.dto;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
