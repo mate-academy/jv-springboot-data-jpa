6776f87655155541817b349592bdb33e24e6cd2d
Andrew Miroshnikov <giggle2594@gmail.com>
2023-01-03 14:43:00 +0200

    Final refactoring

--- src/main/java/mate/academy/springboot/datajpa/dto/CategoryRequestDto.java
+++ src/main/java/mate/academy/springboot/datajpa/dto/CategoryRequestDto.java
@@ -1,8 +1,10 @@
 package mate.academy.springboot.datajpa.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class CategoryRequestDto {
     private String name;
 }
