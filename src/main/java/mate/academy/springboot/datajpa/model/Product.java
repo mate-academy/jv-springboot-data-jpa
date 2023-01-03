9dbf465dbb7af1ba22612c872ed66f1e4f2a1e01
Andrew Miroshnikov <giggle2594@gmail.com>
2023-01-03 14:29:31 +0200

    Refactor after review

--- src/main/java/mate/academy/springboot/datajpa/model/Product.java
+++ src/main/java/mate/academy/springboot/datajpa/model/Product.java
@@ -1,15 +1,21 @@
 package mate.academy.springboot.datajpa.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "products")
 public class Product {
@@ -18,7 +24,8 @@ public class Product {
     private Long id;
     private String title;
     private BigDecimal price;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
     private Category category;
 
 }
