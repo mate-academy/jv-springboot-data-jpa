504e8873ab9817c56f33ca7f8b3bf9ad5c748e06
Pavlo.Polovyi <pasha32662@gmail.com>
2023-01-03 11:36:59 +0200

    made changes according to review

--- src/main/java/mate/academy/springboot/datajpa/dto/CategoryRequestDto.java
+++ src/main/java/mate/academy/springboot/datajpa/dto/CategoryRequestDto.java
@@ -1,8 +1,11 @@
 package mate.academy.springboot.datajpa.dto;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
+@Setter(AccessLevel.NONE)
 public class CategoryRequestDto {
     private String name;
 }
--- src/main/java/mate/academy/springboot/datajpa/dto/ProductRequestDto.java
+++ src/main/java/mate/academy/springboot/datajpa/dto/ProductRequestDto.java
@@ -1,9 +1,12 @@
 package mate.academy.springboot.datajpa.dto;
 
 import java.math.BigDecimal;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
+@Setter(AccessLevel.NONE)
 public class ProductRequestDto {
     private String title;
     private BigDecimal price;
