cd6bb1042b570963c3408ce423b9a80ef70c85a5
apc10bbe <poka4ailo.oleg@gmail.com>
2023-01-03 22:39:58 +0200

    released REST app used Spring Boot

---
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/CategoryRequestDto.java
@@ -0,0 +1,8 @@
+package mate.academy.springboot.datajpa.model.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryRequestDto {
+    private String name;
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/mapper/ProductMapper.java
@@ -0,0 +1,35 @@
+package mate.academy.springboot.datajpa.model.dto.mapper;
+
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.model.dto.ProductRequestDto;
+import mate.academy.springboot.datajpa.model.dto.ProductResponseDto;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    private final CategoryService categoryService;
+
+    public ProductMapper(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    public ProductResponseDto toDto(Product product) {
+        ProductResponseDto dto = new ProductResponseDto();
+        dto.setId(product.getId());
+        dto.setTitle(product.getTitle());
+        dto.setPrice(product.getPrice());
+        dto.setCategoryId(product.getCategory().getId());
+        return dto;
+    }
+
+    public Product toModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        Category category = categoryService.get(dto.getCategoryId());
+        product.setCategory(category);
+        return product;
+    }
+}
