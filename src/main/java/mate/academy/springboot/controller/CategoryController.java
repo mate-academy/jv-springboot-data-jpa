a0a1ab58bed5c3be073b9e9e65440b95467d4961
galenkoigor <galenkoigor@gmail.com>
2023-01-03 18:30:10 +0100

    Implemented category controller

--- src/main/java/mate/academy/springboot/controller/CategoryController.java
+++ src/main/java/mate/academy/springboot/controller/CategoryController.java
@@ -1,4 +1,51 @@
 package mate.academy.springboot.controller;
 
+import mate.academy.springboot.model.Category;
+import mate.academy.springboot.model.dto.request.CategoryRequestDto;
+import mate.academy.springboot.model.dto.response.CategoryResponseDto;
+import mate.academy.springboot.service.CategoryService;
+import mate.academy.springboot.service.mapper.CategoryMapper;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @PostMapping
+    public CategoryResponseDto add(@RequestBody CategoryRequestDto requestDto) {
+        Category category = categoryService.add(categoryMapper.dtoToModel(requestDto));
+        return categoryMapper.modelToDto(category);
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponseDto getById(@PathVariable Long id) {
+        return categoryMapper.modelToDto(categoryService.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestParam CategoryRequestDto requestDto) {
+        Category category = categoryMapper.dtoToModel(requestDto);
+        category.setId(id);
+        categoryService.update(category);
+    }
 }
--- src/main/java/mate/academy/springboot/model/Category.java
+++ src/main/java/mate/academy/springboot/model/Category.java
@@ -21,7 +21,6 @@ import lombok.ToString;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
     private Long id;
     private String name;
 
