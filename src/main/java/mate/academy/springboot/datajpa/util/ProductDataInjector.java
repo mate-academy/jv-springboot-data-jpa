26ccac2cf160deea605a787184f85daebb9cbe52
Riki Maru <qui.ego@icloud.com>
2023-02-09 16:25:08 +0200

    created products data injector

---
+++ src/main/java/mate/academy/springboot/datajpa/util/ProductDataInjector.java
@@ -0,0 +1,45 @@
+package mate.academy.springboot.datajpa.util;
+
+import java.math.BigDecimal;
+import java.util.Random;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import mate.academy.springboot.datajpa.service.ProductService;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class ProductDataInjector {
+    private static final int count = 150;
+    private static final String[] categories = {
+            "Grocery",
+            "Alcohol",
+            "Drink",
+            "Confectionery",
+            "Baking",
+            "Meat",
+            "Fish",
+            "Tobacco"
+    };
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    @PostConstruct
+    public void init() {
+        for (String category : categories) {
+            categoryService.save(Category.builder()
+                    .name(category).build());
+        }
+        for (int i = 1; i <= count; i++) {
+            productService.save(Product.builder()
+                    .title("Product#" + i)
+                    .price(BigDecimal.valueOf(new Random().nextLong(500L) + 1L))
+                    .category(categoryService.findById(new Random()
+                            .nextLong(categories.length) + 1L))
+                    .build());
+        }
+    }
+}
