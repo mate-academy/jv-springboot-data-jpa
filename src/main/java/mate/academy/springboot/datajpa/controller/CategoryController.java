94666b526f33b7993ee3a5a3201211fc605694e9
AndriySichniy <sichniy.andriy@gmail.com>
2023-03-01 14:27:12 +0200

    add controllers

---
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -0,0 +1,49 @@
+package mate.academy.springboot.datajpa.controller;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/create")
+    public Category create(@RequestBody Category category) {
+        return categoryService.create(category);
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody Category category) {
+        categoryService.update(category);
+    }
+
+    @GetMapping("/all")
+    public List<Category> getAll() {
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Category> getById(@PathVariable Long id) {
+        return categoryService.getById(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        categoryService.deleteById(id);
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
@@ -0,0 +1,65 @@
+package mate.academy.springboot.datajpa.controller;
+
+import jakarta.websocket.server.PathParam;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/create")
+    public Product create(@RequestBody Product product) {
+        return productService.create(product);
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody Product product) {
+        productService.update(product);
+    }
+
+    @GetMapping("/all")
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getById(@PathVariable Long id) {
+        return productService.getById(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+
+    @GetMapping("/price")
+    public List<Product> getByPriceBetween(@PathParam("price")BigDecimal from,
+                                           @PathParam("price") BigDecimal to) {
+        return productService.getAllBetweenPrice(from, to);
+    }
+
+    @GetMapping("/category")
+    public List<Product> getProductsFromCategory(@PathParam("category") Category category) {
+        return productService.getByCategory(category);
+    }
+}
