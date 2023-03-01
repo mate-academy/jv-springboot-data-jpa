4c890e86e47fc648324bc640634a257ed6616175
AndriySichniy <89684976+SichniyAndriy@users.noreply.github.com>
2023-03-01 14:51:19 +0200

    Update src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
 
