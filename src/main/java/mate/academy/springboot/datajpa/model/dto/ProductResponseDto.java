82eeb3c163c7a85c7cf1aa9bb3f44dad93d0063c
AndriySichniy <sichniy.andriy@gmail.com>
2023-03-01 14:33:02 +0200

    remove prot annotations

--- src/main/java/mate/academy/springboot/datajpa/model/dto/ProductResponseDto.java
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/ProductResponseDto.java
@@ -5,15 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import mate.academy.springboot.datajpa.model.Category;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ProductResponseDto {
     private Long id;
     private String title;
