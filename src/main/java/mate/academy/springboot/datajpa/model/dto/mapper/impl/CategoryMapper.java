1cff508a3786d0bc0fa028667e55aa868367c335
Dastya <daniillipenec@gmail.com>
2023-03-20 13:29:14 +0200

    first commit

---
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/mapper/impl/CategoryMapper.java
@@ -0,0 +1,27 @@
+package mate.academy.springboot.datajpa.model.dto.mapper.impl;
+
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.dto.mapper.RequestDtoMapper;
+import mate.academy.springboot.datajpa.model.dto.mapper.ResponseDtoMapper;
+import mate.academy.springboot.datajpa.model.dto.request.CategoryRequestDto;
+import mate.academy.springboot.datajpa.model.dto.response.CategoryResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper implements RequestDtoMapper<CategoryRequestDto, Category>,
+        ResponseDtoMapper<CategoryResponseDto, Category> {
+    @Override
+    public Category toModel(CategoryRequestDto dto) {
+        Category category = new Category();
+        category.setName(dto.getName());
+        return category;
+    }
+
+    @Override
+    public CategoryResponseDto toDto(Category model) {
+        CategoryResponseDto responseDto = new CategoryResponseDto();
+        responseDto.setId(model.getId());
+        responseDto.setName(model.getName());
+        return responseDto;
+    }
+}
