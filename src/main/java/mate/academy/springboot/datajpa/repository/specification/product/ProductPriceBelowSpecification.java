ff38ef52c942ad4627689848ca336aed0f0a31c9
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-03-20 22:15:12 +0200

    Add ProductPriceBelowSpecification

---
+++ src/main/java/mate/academy/springboot/datajpa/repository/specification/product/ProductPriceBelowSpecification.java
@@ -0,0 +1,33 @@
+package mate.academy.springboot.datajpa.repository.specification.product;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.criteria.Predicate;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.repository.specification.SpecificationProvider;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductPriceBelowSpecification implements SpecificationProvider<Product> {
+
+    private static final String FILTER_KEY = "priceTo";
+    private static final String FIELD_NAME = "price";
+
+    @Override
+    public Specification<Product> getSpecification(String[] prices) {
+        return (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            for (String value : prices) {
+                Predicate predicate = cb.lessThan(root.get(FIELD_NAME).as(BigDecimal.class),
+                        BigDecimal.valueOf(Long.parseLong(value)));
+                predicates.add(predicate);
+            }
+            return cb.or(predicates.toArray(predicates.toArray(new Predicate[0])));
+        };
+    }
+
+    @Override
+    public String getFilterKey() {
+        return FILTER_KEY;
+    }
+}
