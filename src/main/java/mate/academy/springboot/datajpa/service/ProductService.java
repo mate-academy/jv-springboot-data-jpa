f06b8041b5938429adbf488bf2b0b4acfce27ee6
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-03-20 22:48:24 +0200

    Add ProductService

--- src/main/java/mate/academy/springboot/datajpa/service/ProductService.java
+++ src/main/java/mate/academy/springboot/datajpa/service/ProductService.java
@@ -1,4 +1,13 @@
 package mate.academy.springboot.datajpa.service;
 
-public interface ProductService {
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+
+public interface ProductService extends AbstractService<Product> {
+    List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to);
+
+    List<Product> findAllInCategories(Category... categories);
+
 }
