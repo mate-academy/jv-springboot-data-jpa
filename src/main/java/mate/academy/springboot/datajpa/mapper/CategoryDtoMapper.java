3b599ca021d8ab9d2987096d63202cedb93ea392
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-03-22 11:58:27 +0200

    Add Dto Mapper

---
+++ src/main/java/mate/academy/springboot/datajpa/mapper/CategoryDtoMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.springboot.datajpa.mapper;
+
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.dto.CategoryRequestDto;
+import mate.academy.springboot.datajpa.model.dto.CategoryResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryDtoMapper {
+    public CategoryResponseDto toResponseDto(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName().name());
+        return categoryResponseDto;
+    }
+
+    public Category toModel(CategoryRequestDto dto) {
+        Category category = new Category();
+        category.setName(Enum.valueOf(Category.Name.class, dto.getName()));
+        return category;
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/mapper/ProductDtoMapper.java
@@ -0,0 +1,29 @@
+package mate.academy.springboot.datajpa.mapper;
+
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.model.dto.ProductRequestDto;
+import mate.academy.springboot.datajpa.model.dto.ProductResponseDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDtoMapper {
+    public ProductResponseDto toResponseDto(Product product) {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setTitle(product.getTitle());
+        productResponseDto.setPrice(product.getPrice());
+        productResponseDto.setCategory(product.getCategory().getName().name());
+        return productResponseDto;
+    }
+
+    public Product toModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        Category category = new Category();
+        category.setName(Enum.valueOf(Category.Name.class, dto.getCategory()));
+        product.setCategory(category);
+        return product;
+    }
+}
