7891b293f4d6a10af544152062ca76c574ae4356
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-03-22 11:58:19 +0200

    Add Dto

---
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/CategoryRequestDto.java
@@ -0,0 +1,10 @@
+package mate.academy.springboot.datajpa.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryRequestDto {
+    private String name;
+}
