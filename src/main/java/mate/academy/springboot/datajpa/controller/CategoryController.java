9139ad4b7ac04f8cabf42c4252a69fd8854ccfa2
codered23 <oleg2003andriy@gmail.com>
2023-05-06 18:49:37 +0300

    changed

--- src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 @RequestMapping("/categories")
 public class CategoryController {
     private final CategoryService categoryService;
