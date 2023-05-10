3e2b5e3bcddaa74819dd68eba0c588c8c68b4d24
Oleksii Mospan <alexey.mospan@gmail.com>
2023-05-10 16:10:01 +0200

    Added implementations to CategoryService

--- src/main/java/mate/academy/springboot/datajpa/service/CategoryService.java
+++ src/main/java/mate/academy/springboot/datajpa/service/CategoryService.java
@@ -1,14 +1,12 @@
 package mate.academy.springboot.datajpa.service;
 
 import java.util.List;
-import mate.academy.springboot.datajpa.dto.CategoryResponseDto;
 import mate.academy.springboot.datajpa.model.Category;
-import org.springframework.stereotype.Service;
 
 public interface CategoryService {
     Category save(Category category);
     Category update(Category category);
-    Category delete(Category category);
+    void delete(Long id);
     List<Category> findAll();
 
 }
--- src/main/java/mate/academy/springboot/datajpa/service/CategoryServiceImpl.java
+++ src/main/java/mate/academy/springboot/datajpa/service/CategoryServiceImpl.java
@@ -1,28 +1,41 @@
 package mate.academy.springboot.datajpa.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @Override
     public Category save(Category category) {
-        return null;
+        return categoryRepository.save(category);
     }
 
     @Override
     public Category update(Category category) {
-        return null;
+        Category existingCategory =
+                categoryRepository.findById(category.getId())
+                        .orElseThrow(() -> new EntityNotFoundException("Category with "
+                                                            + category.getId() + " not found"));
+        existingCategory.setName(category.getName());
+        return categoryRepository.save(existingCategory);
     }
 
     @Override
-    public Category delete(Category category) {
-        return null;
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
     }
 
     @Override
     public List<Category> findAll() {
-        return null;
+        return categoryRepository.findAll();
     }
 }
