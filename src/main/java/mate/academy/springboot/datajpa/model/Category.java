42d2b60eb6dbcc6feb43501e17706ac7a45e5627
Oleksii Mospan <alexey.mospan@gmail.com>
2023-05-12 22:33:52 +0200

    Changed Lombok annotations

--- src/main/java/mate/academy/springboot/datajpa/model/Category.java
+++ src/main/java/mate/academy/springboot/datajpa/model/Category.java
@@ -5,11 +5,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
 @Entity
 @Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Category {
 
     @Id
