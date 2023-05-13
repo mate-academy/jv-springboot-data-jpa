bf7efcb46a210e9818e374c9e3e2cbc864e82f9d
Nikita <nsalogub96@gmail.com>
2023-05-13 18:46:47 +0300

    spring data jpa practice done

---
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -0,0 +1,61 @@
+package mate.academy.springboot.datajpa.controller;
+
+import mate.academy.springboot.datajpa.dto.CategoryRequestDto;
+import mate.academy.springboot.datajpa.dto.CategoryResponseDto;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import mate.academy.springboot.datajpa.service.mapper.RequestDtoMapper;
+import mate.academy.springboot.datajpa.service.mapper.ResponseDtoMapper;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final RequestDtoMapper<CategoryRequestDto, Category> requestDtoMapper;
+    private final ResponseDtoMapper<CategoryResponseDto, Category> responseDtoMapper;
+
+    public CategoryController(CategoryService categoryService,
+                              RequestDtoMapper<CategoryRequestDto, Category> requestDtoMapper,
+                              ResponseDtoMapper<CategoryResponseDto, Category> responseDtoMapper) {
+        this.categoryService = categoryService;
+        this.requestDtoMapper = requestDtoMapper;
+        this.responseDtoMapper = responseDtoMapper;
+    }
+
+    @PostMapping
+    public CategoryResponseDto create(@RequestBody CategoryRequestDto requestDto) {
+        return responseDtoMapper
+                .mapToDto(categoryService
+                        .save(requestDtoMapper
+                                .mapToModel(requestDto)));
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponseDto findById(@PathVariable Long id) {
+        return responseDtoMapper
+                .mapToDto(categoryService
+                        .findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponseDto update(@PathVariable Long id,
+                                      @RequestBody CategoryRequestDto requestDto) {
+        Category category = requestDtoMapper.mapToModel(requestDto);
+        category.setId(id);
+        categoryService.update(category);
+        return responseDtoMapper.mapToDto(categoryService.update(category));
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/mapper/ProductMapper.java
@@ -0,0 +1,36 @@
+package mate.academy.springboot.datajpa.service.mapper;
+
+import mate.academy.springboot.datajpa.dto.ProductRequestDto;
+import mate.academy.springboot.datajpa.dto.ProductResponseDto;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper implements ResponseDtoMapper<ProductResponseDto, Product>,
+        RequestDtoMapper<ProductRequestDto, Product> {
+    private final CategoryService categoryService;
+
+    public ProductMapper(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Override
+    public Product mapToModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        product.setCategory(categoryService.findById(dto.getCategoryId()));
+        return product;
+    }
+
+    @Override
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto dto = new ProductResponseDto();
+        dto.setId(product.getId());
+        dto.setTitle(product.getTitle());
+        dto.setPrice(product.getPrice());
+        dto.setCategoryId(product.getCategory().getId());
+        return dto;
+    }
+}
