de8fae41121c2bd04293c06b30c6932a65185af4
Sushko Roman <formatesus@gmail.com>
2023-05-21 15:12:26 +0300

    created services

---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import lombok.AllArgsConstructor;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.repository.CategoryRepository;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category findById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Can`t find Category by id " + id));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Category update(Category category) {
+        return categoryRepository.save(category);
+    }
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.repository.ProductRepository;
+import mate.academy.springboot.datajpa.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Can`t find Product by id " + id));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to) {
+        return productRepository.findAllByPriceBetween(from, to);
+    }
+
+    @Override
+    public List<Product> findProductsByCategoryNameIn(List<String> categories) {
+        return productRepository.findProductsByCategoryNameIn(categories);
+    }
+}
