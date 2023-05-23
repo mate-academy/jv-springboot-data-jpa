fc4807df3b4ae1a0e233de3669c84274e3636818
Nimakel <maxim.yashyn@gmail.com>
2023-05-23 12:19:27 +0300

    created mapper implementations

---
+++ src/main/java/mate/academy/springboot/datajpa/service/mapper/ProductMapper.java
@@ -0,0 +1,26 @@
+package mate.academy.springboot.datajpa.service.mapper;
+
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.model.dto.request.ProductRequestDto;
+import mate.academy.springboot.datajpa.model.dto.response.ProductResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper implements Mapper<Product, ProductRequestDto, ProductResponseDto> {
+    public Product toModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        product.setCategory(dto.getCategory());
+        return product;
+    }
+
+    public ProductResponseDto toDto (Product product) {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setTitle(product.getTitle());
+        productResponseDto.setPrice(product.getPrice());
+        productResponseDto.setCategory(product.getCategory());
+        return productResponseDto;
+    }
+}
