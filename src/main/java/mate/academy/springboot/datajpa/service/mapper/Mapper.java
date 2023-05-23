9e05354d3976fd05b9c860030f74c37f1ef49bae
Nimakel <maxim.yashyn@gmail.com>
2023-05-23 19:17:19 +0300

    added Javadoc

--- src/main/java/mate/academy/springboot/datajpa/service/mapper/Mapper.java
+++ src/main/java/mate/academy/springboot/datajpa/service/mapper/Mapper.java
@@ -1,5 +1,12 @@
 package mate.academy.springboot.datajpa.service.mapper;
 
+/**
+ *
+ * @param <M> - model of entity
+ * @param <D> - request dto of entity
+ * @param <F> - response dto of entity
+ */
+
 public interface Mapper<M, D, F> {
     M toModel(D requestDto);
 
