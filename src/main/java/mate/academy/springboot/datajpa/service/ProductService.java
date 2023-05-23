a8dde266665c9a1b74f71e21e35059afe5092778
Nimakel <maxim.yashyn@gmail.com>
2023-05-23 12:18:00 +0300

    created services

---
+++ src/main/java/mate/academy/springboot/datajpa/service/ProductService.java
@@ -0,0 +1,20 @@
+package mate.academy.springboot.datajpa.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import mate.academy.springboot.datajpa.model.Product;
+
+public interface ProductService {
+    Product save(Product product);
+
+    Product get(Long id);
+
+    boolean delete(Long id);
+
+    Product update(Product product);
+
+    List<Product> findAllByPriceBetween(BigDecimal priceOne, BigDecimal priceTwo);
+
+    List<Product> findAllInCategories(Map<String, String> params);
+}
