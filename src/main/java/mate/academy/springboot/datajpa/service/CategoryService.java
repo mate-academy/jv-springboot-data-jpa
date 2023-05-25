ede35d7fabf8315e48f3cad3047ba1b9fb47cf6c
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-05-25 23:24:31 +0300

    create CategoryRepository and CategoryService

---
+++ src/main/java/mate/academy/springboot/datajpa/service/CategoryService.java
@@ -0,0 +1,14 @@
+package mate.academy.springboot.datajpa.service;
+
+import java.util.Optional;
+import mate.academy.springboot.datajpa.model.Category;
+
+public interface CategoryService {
+    Category save(Category category);
+
+    Category get(Long id);
+
+    void delete(Long id);
+
+    Category update(Category category);
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,36 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import mate.academy.springboot.datajpa.dao.CategoryRepository;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category get(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() ->
+                new NoSuchElementException("Category with id " + id + " hasn't found"));
+    }
+
+    @Override
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Category update(Category category) {
+        return categoryRepository.save(category);
+    }
+}
