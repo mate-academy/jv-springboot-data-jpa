3470901be473696bc3905b9efe03112aca9b3c81
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-05-25 22:35:02 +0300

    create models

---
+++ src/main/java/mate/academy/springboot/datajpa/model/Category.java
@@ -0,0 +1,16 @@
+package mate.academy.springboot.datajpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/model/Product.java
@@ -0,0 +1,20 @@
+package mate.academy.springboot.datajpa.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    @ManyToOne
+    private Category category;
+}
