71427378db63cfb28fd4562c564cf089c6844368
Alexander <liero2D@gmail.com>
2023-05-26 02:41:12 +0300

    Description. Added:required dependencies, H2 databases configuration. Created Product and Category: Controllers, Mappers, DTO, Classes Repositories, Services. Added all methods and interfaces.

---
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.springboot.datajpa.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.repository.ProductRepository;
+import mate.academy.springboot.datajpa.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository repository) {
+        this.productRepository = repository;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product get(Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to) {
+        return productRepository.findAllByPriceBetween(from, to);
+    }
+
+    @Override
+    public List<Product> getAllByCategory(List<String> category) {
+        return productRepository.getAllByCategoryNameIn(category);
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
