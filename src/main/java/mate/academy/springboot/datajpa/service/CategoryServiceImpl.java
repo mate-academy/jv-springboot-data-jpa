c3beb3d98f3fc24337f58e1b5bebdb730e52ce85
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-05-31 11:10:02 +0200

    task springboot-data-jpa is done

---
+++ src/main/java/mate/academy/springboot/datajpa/service/CategoryServiceImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.springboot.datajpa.service;
+
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Can't get product by id: " + id));
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+}
