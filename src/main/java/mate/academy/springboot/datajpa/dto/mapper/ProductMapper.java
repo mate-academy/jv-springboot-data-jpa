492cfeddd9e6252b0fb5caec7b2f32ae1aaa7d39
Max Lutovinov <maxlutovinov@gmail.com>
2023-06-03 20:42:24 +0300

    Create DTOs and mappers

---
+++ src/main/java/mate/academy/springboot/datajpa/dto/mapper/ProductMapper.java
@@ -0,0 +1,38 @@
+package mate.academy.springboot.datajpa.dto.mapper;
+
+import mate.academy.springboot.datajpa.dto.ProductRequestDto;
+import mate.academy.springboot.datajpa.dto.ProductResponseDto;
+import mate.academy.springboot.datajpa.model.Product;
+import mate.academy.springboot.datajpa.service.CategoryService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper implements RequestDtoMapper<ProductRequestDto, Product>,
+        ResponseDtoMapper<Product, ProductResponseDto> {
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    public ProductMapper(CategoryService categoryService, CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @Override
+    public Product mapToModel(ProductRequestDto requestDto) {
+        Product product = new Product();
+        product.setTitle(requestDto.getTitle());
+        product.setPrice(requestDto.getPrice());
+        product.setCategory(categoryService.get(requestDto.getCategoryId()));
+        return product;
+    }
+
+    @Override
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto responseDto = new ProductResponseDto();
+        responseDto.setId(product.getId());
+        responseDto.setTitle(product.getTitle());
+        responseDto.setPrice(product.getPrice());
+        responseDto.setCategory(categoryMapper.mapToDto(product.getCategory()));
+        return responseDto;
+    }
+}
