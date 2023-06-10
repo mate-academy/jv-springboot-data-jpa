c0e8261abef04e4981c767aee5077662f4a58cd4
Skrudj <ssinilov@gmail.com>
2023-06-10 19:47:53 +0300

    add repositories

---
+++ src/main/java/mate/academy/springboot/datajpa/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package mate.academy.springboot.datajpa.repository;
+
+import mate.academy.springboot.datajpa.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
