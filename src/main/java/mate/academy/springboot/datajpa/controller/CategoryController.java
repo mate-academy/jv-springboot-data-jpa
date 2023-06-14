566b604c5a2b7beebd1ec6891edb954ce9f28b5e
Serhii Shyian <serhii.shn@gmail.com>
2023-06-14 15:00:41 +0300

    1) Added @RequestMapping annotation (/categories) to CategoryController

--- src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
+++ src/main/java/mate/academy/springboot/datajpa/controller/CategoryController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/categories")
 public class CategoryController {
     private final CategoryService categoryService;
     private final DtoMapper<Category, CategoryRequestDto, CategoryResponseDto> dtoMapper;
