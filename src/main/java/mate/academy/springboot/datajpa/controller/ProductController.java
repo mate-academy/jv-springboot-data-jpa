e7634c75d6dcab0faa4990b823d1e02c6c6599d1
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-07-06 19:11:41 +0300

    Created DTOs, Mappers and Services

---
+++ src/main/java/mate/academy/springboot/datajpa/controller/ProductController.java
@@ -0,0 +1,10 @@
+package mate.academy.springboot.datajpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+}
---
+++ src/main/java/mate/academy/springboot/datajpa/service/ProductService.java
@@ -0,0 +1,21 @@
+package mate.academy.springboot.datajpa.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.springboot.datajpa.model.Category;
+import mate.academy.springboot.datajpa.model.Product;
+
+public interface ProductService {
+    Product save(Product product);
+
+    Product get(Long id);
+
+    void delete(Long id);
+
+    Product update(Product product);
+
+    List<Product> getAllBetweenPrice(BigDecimal from, BigDecimal to);
+
+    List<Product> findAllByCategoryIn(List<Category> categories);
+
+}
