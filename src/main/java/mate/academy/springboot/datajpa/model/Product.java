dd667261dc1b8146127ec5e8c17a09d82727c015
Sergey gordeichuk <ya.gordeichuk@gmail.com>
2023-07-06 11:09:06 +0300

    Created Product.class and Category.class

---
+++ src/main/java/mate/academy/springboot/datajpa/model/Product.java
@@ -0,0 +1,24 @@
+package mate.academy.springboot.datajpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Set;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    @OneToMany
+    private Set<Category> category;
+}
