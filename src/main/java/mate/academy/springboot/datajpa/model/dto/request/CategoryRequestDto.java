a27aa7b320a75b355e9d29a698a801dc1cacf548
Ponomar <artemponomar98@gmail.com>
2023-07-12 18:31:52 +0300

    added validation

--- src/main/java/mate/academy/springboot/datajpa/model/dto/request/CategoryRequestDto.java
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/request/CategoryRequestDto.java
@@ -1,9 +1,13 @@
 package mate.academy.springboot.datajpa.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 public class CategoryRequestDto {
     private Long id;
+    @NotBlank
     private String name;
 }
--- src/main/java/mate/academy/springboot/datajpa/model/dto/request/ProductRequestDto.java
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/request/ProductRequestDto.java
@@ -1,12 +1,20 @@
 package mate.academy.springboot.datajpa.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
 @Data
+@Validated
 public class ProductRequestDto {
+    @Positive
     private Long id;
+    @NotBlank
     private String title;
+    @Positive
     private BigDecimal price;
+    @NotBlank
     private String category;
 }
