feb9ad44d08218aef3c340fadc6a0cdc006f6967
Vladyslav Krutyus <kijdron@gmail.com>
2023-07-15 11:54:58 +0300

    validation added

--- src/main/java/mate/academy/springboot/datajpa/model/dto/request/ProductRequestDto.java
+++ src/main/java/mate/academy/springboot/datajpa/model/dto/request/ProductRequestDto.java
@@ -1,11 +1,16 @@
 package mate.academy.springboot.datajpa.model.dto.request;
 
 import java.math.BigDecimal;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class ProductRequestDto {
+    @NotNull
     private String title;
+    @Positive
     private BigDecimal price;
+    @Positive
     private Long categoryId;
 }
