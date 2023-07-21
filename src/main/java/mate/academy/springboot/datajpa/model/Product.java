b2cd3e13ddd96d16c01a44224814c31a05652b61
Vladyslav Krutyus <kijdron@gmail.com>
2023-07-21 13:30:37 +0300

    fixed the project

--- src/main/java/mate/academy/springboot/datajpa/model/Product.java
+++ src/main/java/mate/academy/springboot/datajpa/model/Product.java
@@ -1,6 +1,5 @@
 package mate.academy.springboot.datajpa.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
